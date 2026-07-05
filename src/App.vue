2d621fff6f3f8a777bcfff8976b0c7c6176f94fc
Rich-Lawrence <richie.l.lawrence@gmail.com>
2026-07-05 14:49:41 +0100

    Initial Vue app

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import api from '@/services/api.js';
+
+api.get('/test')
+  .then(res => {
+    console.log(res.data);
+  })
+  .catch(err => {
+    console.error(123, err);
+  });
+
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
+
+<!-- DEPLOYMENT -->
+<!-- After npm run build, manually upload contents of dist to server -->
